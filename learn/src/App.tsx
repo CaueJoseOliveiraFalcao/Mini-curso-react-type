bb6aaa3955c2cd71bc70661234e3437282f7fef8
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-02-23 19:06:46 -0400

    k

--- learn/src/App.tsx
+++ learn/src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import  Header  from './components/Header';
 import Footer from './components/Footer';
 export default function App(){
-  const name = 'caue';
+  const [name,setName] = useState('caue');
+  const [age,setAge] = useState(19)
+
+  const [bg,setBg] = useState("#FF0000")
+  const anovinte = () => {
+    setAge(20)
+  }
+  const anonoventa = () => {
+    setAge(90)
+  }
+
   return(
-    <div>
-      <Header name={name}/>
+    <div style={{backgroundColor:bg}}>
+      <Header name={name} age={age}/>
+      <button onClick={anovinte}>tenho 20</button>
+      <button onClick={anonoventa}>tenho 90</button>
+      
       <Footer/>
     </div>
+    
   )
 }
 
--- learn/src/components/Header.tsx
+++ learn/src/components/Header.tsx
@@ -1,10 +1,11 @@
 type Props = {
     name:string;
+    age:number;
 }
 export default function Header(props:Props){
     return(
         <header>
-            {props.name}
+            {props.name},{props.age}
         </header>
     )
 }
